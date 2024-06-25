a7999b04436b50e31d63bd7e4dc0efb48f7e1919
Zainab057 <olutomilolakudayisi@gmail.com>
2024-06-25 12:56:26 +0100

    Create zainab_kudayisi.py
    week 4 task

---
+++ zainab_kudayisi.py
@@ -0,0 +1,21 @@
+u = 9
+v = 10
+a = "goodbye"
+b = "happy birthday"
+c = "How are you?"
+d = 'what do you do?'
+e = 'Hanan is a pretty girl'
+f = 'good morning'
+print(u)
+g = 15
+print(b)
+print(v > u)
+print(a,b,c,d,e,f)
+print(u>g)
+print(v)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
