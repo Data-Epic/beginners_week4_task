ad9388d0f1a0819b2ba7c9f6d957c1c9cea93cee
Oluwapelumi <bamideleadeleke36253@gmail.com>
2024-06-25 04:50:58 +0100

    variables added

---
+++ working_with_variables.py
@@ -0,0 +1,18 @@
+first_variable = 101
+second_variable = 102
+third_variable = 'first string'
+fourth_variable = 'second string'
+fifth_variable = "third string"
+sixth_variable = "fourth string"
+seventh_variable = "fifth string"
+eight_variable = "sixth string"
+nineth_variable = 5 > 2
+tenth_variable = 2 > 5
+
+import random
+random_case_1 = (random.randrange(1, 10))
+random_case_2 = (random.randrange(1, 10))
+random_case_3 = (random.randrange(1, 10))
+random_case_4 = (random.randrange(1, 10))
+random_case_5 = (random.randrange(1, 10))
+
