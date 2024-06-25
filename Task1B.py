b2ecd2db4012bf902aaa72480e596301633825ef
Oguinye_Emeka_Gavin <emekagarvin2015@gmail.com>
2024-06-25 16:14:42 +0100

    Gavin commits all saved and tracked dataelements

---
+++ Task1B.py
@@ -0,0 +1,15 @@
+a = "Our deepest fear is not that we are inadequate. Our deepest fear is that we are powerful beyond measure."
+print(type(a))
+b = "The scripts you entered reads: 'I owe you no more money'"
+print(type(b))
+c = "We were born to make manifest the glory of God that is within us. It's not just in some of us; it's in everyone" 
+"And as we let our own light shine, we unconsciously give other people permission to do the same"
+"As we are liberated from our own fear, our presence automatically liberates others"
+print(type(c))
+d = "The_Goat_That_Entered_The_Farm,_Ate_Eveything_That_Was_Planted_In_It"
+print(type(d))
+e = "TheGoatThatEnteredTheFarm,AteEveythingThatWasPlantedInIt"
+print(type(e))
+f = "I have a strong feeling that everything would be okay"
+print(f)
+print(type(f))
---
+++ Task2A.py
@@ -0,0 +1,16 @@
+import random
+random = random.randrange (2, 50)
+print(random)
+A = 22
+
+import random
+random = random.randint (500, 2000)
+print(random)
+
+B = 1772
+
+import _random 
+_random = _random._State (100, 2000)
+print(_random)
+
+C = 1223
