b7dc72601bda77dfdc15dc6860a5505c57c3d5a5
TolieJolie <tolamiseolasehinde@gmail.com>
2024-06-26 17:28:34 +0100

    task for week 4

---
+++ week4.py
@@ -0,0 +1,17 @@
+import random
+num_1 = 2228.71
+num_1 = 99999999996543
+car_brands = "Toyota" 
+car_type = "SUV"
+names = "Iremide"
+player_1 = "James Lord"
+player_2 = "Joshua Banks"
+hair_styles_for_females = "Bohemian braids"
+check_and_balance1 = 20>2
+check_and_balance2 = 100==40
+
+pick_without_thinking = random.randrange(1, 5)
+choose_again = random.randrange(5555, 20000)
+Game_num = random.randrange(0, 50)
+serial_nun = random.randrange(30, 35)
+identity_num = random.randrange(7, 30)
